00351cacfa1163aa5061f9b586c53c47a689c8e8
mritunjaysaha <mritunjaysaha17@gmail.com>
2023-03-20 21:30:47 +0530

    feat: auth routes

--- server/app.js
+++ server/app.js
@@ -5,7 +5,8 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const { connectDB } = require("./config/db");
 
-
+// routes
+const authRoutes = require("./routes/auth");
 
 // initialize app
 const app = express();
@@ -20,5 +21,6 @@ app.use(express.json({ extend: false }));
 
 // my routes
 app.get("/", (req, res) => res.send("Server is active"));
+app.use("/api/auth", authRoutes);
 
 module.exports = { app };
