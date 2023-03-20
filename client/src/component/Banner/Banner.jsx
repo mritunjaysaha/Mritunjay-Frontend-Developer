cf7feddfb9c28a00a6d0883315c6f5d9f1582547
mritunjaysaha <mritunjaysaha17@gmail.com>
2023-03-21 01:03:30 +0530

    feat: added banner

--- client/src/component/Banner/Banner.jsx
+++ client/src/component/Banner/Banner.jsx
@@ -1,3 +1,22 @@
 export function Banner() {
-    return <section>SKILLS BANNER</section>;
+    const skills = [
+        'React.JS',
+        'Next.JS',
+        'MongoDB',
+        'Express.JS',
+        'Node.JS',
+        'WebPack',
+        'Tailwind CSS',
+        'CSS3',
+        'HTML5',
+        'JavaScript',
+        'SCSS',
+    ];
+    return (
+        <section className='flex h-80 w-full justify-center items-center gap-4'>
+            {skills.map((data) => (
+                <p className='bg-blue-500 h-max p-4 rounded-md text-white font-bold'>{data}</p>
+            ))}
+        </section>
+    );
 }
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import { Banner } from '../../component/Banner/Banner';
 import { DataGrid } from '../../component/DataGrid/DataGrid';
 import { SearchSection } from '../../component/Search/SearchSection';
 
 export default function Home() {
     return (
         <section className='flex flex-col'>
+            <Banner />
             <SearchSection />
             <DataGrid />
         </section>
